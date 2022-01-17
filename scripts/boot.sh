d60dd0726cd717dbfa68120ef7d745784aa116c3
Stephen Neal <stephen.neal14@gmail.com>
2022-01-17 11:15:01 -0500

    FIX boot script by removing upgrade command and fixing docker down command

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -6,16 +6,19 @@ mode=${1-"dev"}
 # Declare the log to follow ('query' or 'laravel')
 log=${2-"query"}
 
-
-# Check if 'upgrade' scripts should be run
-if [[ "${mode}" == "upgrade" ]]; then
-  brew upgrade
-  composer update --no-cache
+# Export Docker image Tag
+if [ -z "$TRAVIS_BRANCH" ]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+else
+    BRANCH="${TRAVIS_BRANCH}"
 fi
+export BRANCH
 
 # Shut down currently running containers
-(docker compose -f docker-compose-dev.yml build && echo "Stopping running containers before creating service...") \
-  & docker compose -f docker-compose-dev.yml down -v --remove-orphans > /dev/null 2>&1
+composer down
+
+# Build new containers
+docker compose -f docker-compose-dev.yml build --progress plain
 
 # Start fresh container instances
 docker compose -f docker-compose-dev.yml up -d
