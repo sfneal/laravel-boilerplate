cbc16348e485893ce954755fed5eeccb6c9e5f1d
Stephen Neal <stephen.neal14@gmail.com>
2022-01-31 14:29:07 -0500

    ADD `WelcomePageTest` that executes a basic status test

---
+++ tests/Unit/WelcomePageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class WelcomePageTest extends TestCase
+{
+    /** @test */
+    public function status_is_okay()
+    {
+        $response = $this->get('/');
+        
+        $response->assertStatus(200);
+        $response->assertOk();
+    }
+}
