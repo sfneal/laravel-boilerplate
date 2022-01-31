e031d50796699e9915399d169a189e95ebcea541
Stephen Neal <stephen.neal14@gmail.com>
2022-01-31 14:56:55 -0500

    ADD yarn js jquery dependencies & added to webpack bundle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+// Bootstrap, jQuery, popper
 require('./bootstrap');
+
+// JS Global Compulsory
+require('jquery-migrate/dist/jquery-migrate.min.js');
+
+// Custom
+require('./custom.js');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,13 @@
-window._ = require('lodash');
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+require('jquery-ui');
+require('bootstrap');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
