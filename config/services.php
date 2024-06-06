0dbad3e289a773070abe8f800715520ebb540a6a
Stephen Neal <stephen.neal14@gmail.com>
2024-06-06 13:14:02 -0400

    Apply fixes from StyleCI (#61)

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-//        'model' => Sfneal\Users\Models\User::class,
+        //        'model' => Sfneal\Users\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
