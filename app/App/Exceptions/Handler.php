b5c25231c5c3db24d5a9e4612ad388f9ee9a612b
Stephen Neal <stephen.neal14@gmail.com>
2024-09-17 13:02:26 -0400

    Update Handler.php
    - add Handler.php from laravel/framework v10 (https://github.com/laravel/laravel/blob/10.x/app/Exceptions/Handler.php)

--- app/App/Exceptions/Handler.php
+++ app/App/Exceptions/Handler.php
@@ -2,108 +2,29 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Throwable;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Run;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
+     * The list of the inputs that are never flashed to the session on validation exceptions.
      *
-     * @var array
-     */
-    protected $dontReport = [
-
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
+     * @var array<int, string>
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
-
+    
     /**
-     * Report or log an exception.
-     *
-     * @param  Throwable  $e
-     * @return void
-     *
-     * @throws Throwable
-     * @throws Exception
+     * Register the exception handling callbacks for the application.
      */
-    public function report(Throwable $e)
+    public function register(): void
     {
-        parent::report($e);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  Request  $request
-     * @param  Throwable  $e
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws Throwable
-     */
-    public function render($request, Throwable $e)
-    {
-        // Unauthenticated user, redirect to error page
-        if ($e instanceof AuthenticationException) {
-            return $this->unauthenticated($request, $e);
-        }
-
-        // Debug enabled, display whoops error page
-        if (config('app.debug')) {
-            return $this->renderExceptionWithWhoops($e);
-        }
-
-        // Debug disabled, redirect to error page
-        return parent::render($request, $e);
-    }
-
-    /**
-     * Render a pretty whoops exception for debugging.
-     *
-     * @param  Throwable  $e
-     * @return Response|string
-     */
-    protected function renderExceptionWithWhoops(Throwable $e)
-    {
-        $whoops = new Run();
-        $whoops->pushHandler(new PrettyPageHandler());
-
-        return new Response(
-            $whoops->handleException($e),
-            $e->getStatusCode(),
-            $e->getHeaders()
-        );
-    }
-
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  Request  $request
-     * @param  AuthenticationException  $exception
-     * @return JsonResponse|RedirectResponse
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 }
